aa590c8b153c6ae3c547b5d8f8764f4831f9112b
JakeG2004 <jake.lgendreau@gmail.com>
2025-03-30 14:09:18 -0700

    Fixed gravity gate

--- Assets/Scripts/Environ/GravityGate.cs
+++ Assets/Scripts/Environ/GravityGate.cs
@@ -5,7 +5,7 @@ public class GravityGate : MonoBehaviour
     private enum GateType
     {
         P1,
-        P2,
+        P2,              
         All
     }
 
@@ -13,9 +13,17 @@ public class GravityGate : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if((other.gameObject.tag == "Player1" && _gateType == GateType.P1) || (other.gameObject.tag == "Player2" && _gateType == GateType.P2))
+        if ((other.CompareTag("Player1") && _gateType == GateType.P1) || (other.CompareTag("Player2") && _gateType == GateType.P2) || _gateType == GateType.All)
         {
             other.GetComponent<PlayerController>().FlipGravity();
+        }
+        else if ((other.CompareTag("Player1") && _gateType == GateType.P2) || (other.CompareTag("Player2") && _gateType == GateType.P1))
+        {
+            Rigidbody2D otherRB = other.GetComponent<Rigidbody2D>();
+            otherRB.linearVelocityX *= -1.1f;
+            
+            Transform otherTransform = other.transform;
+            otherTransform.position += new Vector3(Mathf.Sign(otherRB.linearVelocityX) * 0.1f, 0, 0);
         }
     }
 }

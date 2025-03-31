02ccd622a3674f2fa56226c49d1bd7fbef4533e2
Tavin Yorgason <tazasor@gmail.com>
2025-03-31 00:02:15 -0700

    final commit

--- Assets/Scripts/UI/MusciManager.cs
+++ Assets/Scripts/UI/MusciManager.cs
@@ -2,12 +2,28 @@ using UnityEngine;
 
 public class MusciManager : MonoBehaviour
 {
+    private bool keepPlaying = false;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameObject[] musicObjs = GameObject.FindGameObjectsWithTag("Music");
-
-        if (musicObjs.Length > 1)
+		
+//		if(this.gameObject.GetComponent<AudioSource>().clip.ToString() != musicObjs.clip.ToString())
+//		{
+			for(int i = 0; i < musicObjs.Length; i++)
+			{
+				if(musicObjs[i] != gameObject
+				&& this.gameObject.GetComponent<AudioSource>().clip.ToString() != musicObjs[i].GetComponent<AudioSource>().clip.ToString())
+				{
+					Destroy(musicObjs[i]);
+					keepPlaying = true;
+				}
+	//			Debug.Log(musicObjs[i].GetComponent<AudioSource>().clip.toString());
+			}
+//		}
+//		if(this.gameObject.GetComponent<AudioSource>().clip.toString() != musicObjs
+        if (musicObjs.Length > 1 && !keepPlaying)
         {
             Destroy(this.gameObject);
         }

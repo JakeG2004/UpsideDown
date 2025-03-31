9e9382d2bdd3695b5c01d87a85fd43fa3e4c5ca7
Tavin Yorgason <tazasor@gmail.com>
2025-03-30 23:34:00 -0700

    bug fix

--- Assets/Scripts/UI/MusciManager.cs
+++ Assets/Scripts/UI/MusciManager.cs
@@ -5,14 +5,14 @@ public class MusciManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject[] musicObjs = GameObject.FindGameObjectsWithTag("music");
+        GameObject[] musicObjs = GameObject.FindGameObjectsWithTag("Music");
 
-        if (objs.Length > 1)
+        if (musicObjs.Length > 1)
         {
             Destroy(this.gameObject);
         }
     	
-    	DontDestroyOnLoad(music);
+    	DontDestroyOnLoad(this.gameObject);
     }
 
     // Update is called once per frame
